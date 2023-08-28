917c22487ad0984f8579fa58c43ec68361508e44
russell258 <duckie258@gmail.com>
2023-08-28 14:59:16 +0800

    bare bones

--- src/app/task/task-create/task-create.component.ts
+++ src/app/task/task-create/task-create.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,19 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class TaskCreateComponent implements OnInit{
 
   taskForm: FormGroup; //* Step 1
+  taskArray: any[]=[];
+
+  lowPriority:string="Low";
 
   constructor(private fb: FormBuilder){}
 
   ngOnInit(){
     this.taskForm = this.fb.group({
-    description: this.fb.control<string>(''),
-    priority: this.fb.control<string>(''),
-    due: this.fb.control<Date>(new Date())
+    description: this.fb.control<string>('',[Validators.required, Validators.minLength(5)]),
+    priority: this.fb.control<string>(this.lowPriority,[Validators.required]),
+    due: this.fb.control<Date>(new Date(),[Validators.required])
   })
   }
 
   addForm(){
-    console.log("add clicked");
+    console.log('addForm: '+JSON.stringify(this.taskForm.value))
+    this.taskArray.push(this.taskForm.value)
   }
 
 }
